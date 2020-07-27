e6ae71c64d6376760c97059cab21f3d4db447cb9
Sergey Pushkovskiy <sergey@MacBook-Pro-User.local>
2020-07-27 17:32:20 +0300

    Updates

---
+++ precache-manifest.5dff4c4e1611a501d2d3b5a7058d63f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f13b4ca83c024534532b364c920630",
+    "url": "/yalantis-test/index.html"
+  },
+  {
+    "revision": "bcb3d9aceb6efb5d3fd4",
+    "url": "/yalantis-test/static/css/main.05725cf8.chunk.css"
+  },
+  {
+    "revision": "8e4ca26a62ef47ec9f9d",
+    "url": "/yalantis-test/static/js/2.ae888d7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yalantis-test/static/js/2.ae888d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb3d9aceb6efb5d3fd4",
+    "url": "/yalantis-test/static/js/main.3ca51119.chunk.js"
+  },
+  {
+    "revision": "2fc1cf1aa20128622334",
+    "url": "/yalantis-test/static/js/runtime-main.3867063a.js"
+  }
+]);
